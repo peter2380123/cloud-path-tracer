2e4999bfcf5b925c490cef0457f2e64a58d96f5f
Peter C <peter2380123@gmail.com>
2019-10-22 21:10:25 +1000

    Add error handling upon bad RedisCache connection. (#18)
    * Stop clients from retry bad connection
    
    * Fix trying to set client headers after sending warning.
    
    * Error handling now working and displayed to client

--- controller/routes/post.js
+++ controller/routes/post.js
@@ -1,3 +1,4 @@
+"use strict";
 var express = require('express');
 var multer = require('multer');
 var path = require('path');
@@ -23,43 +24,60 @@ var storage = multer.diskStorage({
 
 var upload = multer({storage: storage})
 
-async function testCache(file){
+function testCache(file) {
+  return new Promise((resolve, reject) => {
+    // Connect to the Azure Cache for Redis over the SSL port using the key.
+    var cacheConnection = redis.createClient(6380, process.env.REDISCACHEHOSTNAME,{auth_pass: process.env.REDISCACHEKEY, tls: {servername: process.env.REDISCACHEHOSTNAME}});
 
-  // Connect to the Azure Cache for Redis over the SSL port using the key.
-  var cacheConnection = redis.createClient(6380, process.env.REDISCACHEHOSTNAME,{auth_pass: process.env.REDISCACHEKEY, tls: {servername: process.env.REDISCACHEHOSTNAME}});
-  console.log(__dirname)
-  
-  let filepath = path.join(__dirname, '..', 'uploads', file)
-  fs.readFile(filepath,'utf8', async function(err, data) {
-    if(err){
-      throw err;
-    }
-    content = data;
+    cacheConnection.on('error', function(err){
+      console.log("Connection error.")
+      reject(err);
+      cacheConnection.quit()
+    });
 
-    console.log("\nCache command: SET Message");
-    console.log("Cache respone: " +  await cacheConnection.setAsync(file, content));
-    
-    console.log("\nCache command: GET Message");
-    console.log("Cache response : " +  await cacheConnection.getAsync(file));
+    let filepath = path.join(__dirname, '..', 'uploads', file);
 
-    console.log("\nDeleting temp upload from server...")
-    
-    fs.unlink(filepath, (err) => {
-      if(err) {
-        throw err;
+    fs.readFile(filepath,'utf8', async function(err, data) {
+
+      try {
+        content = data;
+
+        console.log("\nCache command: SET Message");
+        console.log("Cache respone: " +  await cacheConnection.setAsync(file, content));
+
+        console.log("\nCache command: GET Message");
+        console.log("Cache response : " +  await cacheConnection.getAsync(file));
+
+        console.log("\nDeleting temp upload from server...")
+
+      } catch(e) {
+        reject(e);
       }
-      console.log("Temporary upload deleted")
+      fs.unlink(filepath, (err) => {
+        if(err){
+          reject(err);
+        }
+        console.log("Temporary upload deleted")
+      })
     })
-  })   
+  });
 }
+
+
 
 /* GET users listing. */
 router.post('/', upload.any(), function(req, res, next) {
   console.log("filename is: " + req.files[0].filename)
 
-  testCache(req.files[0].filename);
-  uniqueID = String(req.files[0].filename).split("+", 1)
-  res.render('post', { title: 'Data has been uploaded', uuid: uniqueID });
+  testCache(req.files[0].filename)
+    .then(() => {
+      uniqueID = String(req.files[0].filename).split("+", 1)
+      res.render('post', { title: 'Online Path Tracer', uuid: uniqueID });
+    }).catch(error => {
+      console.log(error)
+      res.render('upload-fail', {title: 'Online Path Tracer', error_msg: error, error_code: JSON.stringify(error)})
+      console.log("res.render complete")
+    });
 });
 
 module.exports = router;
